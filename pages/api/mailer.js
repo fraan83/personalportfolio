91c33f5acfe453aa95f1dc7733e6af91e58117c8
fraan83 <fraan83@gmail.com>
2021-07-05 15:11:01 +0200

    mailreasjust

--- pages/api/mailer.js
+++ pages/api/mailer.js
@@ -1,19 +1,18 @@
 const nodemailer = require('nodemailer')
 
 export default async function sendMail(req, res) {
-  
-  
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smpt.gmail.com',
+
+
+  let transporter = nodemailer.createTransport({
+    host: 'authsmtp.securemail.pro',
     port: 465,
     auth: {
-      user: process.env.emailUser,
-      pass: process.env.emailPassword,
+      user: 'fgreco@craon.it',
+      pass: 'Cinisello4'
     }
   })
 
-  debugger 
+  debugger
   transporter.sendMail({
     from: req.body.email,
     to: "fraan83@gmail.com",
@@ -23,12 +22,12 @@ let transporter = nodemailer.createTransport({
    <h4> Testo del Messaggio: </h4> <br>
    <p> ${req.body.messaggio} </p> <br>
    <p> Conttato mail: ${req.body.email} </p>`,
-  
+
   }).then((response) => {
     res.send(response)
- 
+
   }).catch((error) => { res.send(error) })
- 
+
 }
 
 
